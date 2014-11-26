9d50a4565f1b637a55aeda681493996ae212c6ae
Ido Feins <ifeins@gmail.com>
2014-11-26 16:34:12 +0200

    added gulp-nodemon to start the server.

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,8 @@
-var gulp = require('gulp')
+var gulp = require('gulp');
+var nodemon = require('gulp-nodemon');
+
+gulp.task('develop', function() {
+  nodemon({
+    script: './bin/www'
+  });
+});

e5f2f6f73dbb05839b9f6a89df827b16d9aec3bd
Ido Feins <ifeins@gmail.com>
2014-11-29 18:18:57 +0200

    added gulp coffee tasks that compiles client-side coffeescript to javascript.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,13 @@
 var gulp = require('gulp');
 var nodemon = require('gulp-nodemon');
+var coffee = require('gulp-coffee');
+var gutil = require('gulp-util');
+
+gulp.task('coffee', function() {
+  gulp.src('./app/assets/javascripts/**/*.coffee')
+    .pipe(coffee({bare: true})).on('error', gutil.log)
+    .pipe(gulp.dest('./public/javascripts'))
+});
 
 gulp.task('develop', function() {
   nodemon({

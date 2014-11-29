783e462a66e40c328e6a024b4177c9726bed122f
Ido Feins <ifeins@gmail.com>
2014-11-29 18:27:28 +0200

    added gulp task for scss compilation.

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,21 @@ var gulp = require('gulp');
 var nodemon = require('gulp-nodemon');
 var coffee = require('gulp-coffee');
 var gutil = require('gulp-util');
+var sass = require('gulp-sass');
 
 gulp.task('coffee', function() {
   gulp.src('./app/assets/javascripts/**/*.coffee')
     .pipe(coffee({bare: true})).on('error', gutil.log)
-    .pipe(gulp.dest('./public/javascripts'))
+    .pipe(gulp.dest('./public/javascripts'));
 });
+
+gulp.task('scss', function() {
+  gulp.src('./app/assets/stylesheets/**/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('./public/stylesheets/'));
+});
+
+gulp.task('build', ['coffee', 'scss']);
 
 gulp.task('develop', function() {
   nodemon({

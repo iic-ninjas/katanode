71e43f9b117178dd6c83b6091e581e365979188f
Ido Feins <ifeins@gmail.com>
2014-12-24 18:21:46 +0200

    removed all un-necessary gulp packages.

--- gulpfile.js
+++ gulpfile.js
@@ -1,64 +1,8 @@
 var gulp = require('gulp');
 var nodemon = require('gulp-nodemon');
-var coffee = require('gulp-coffee');
-var gutil = require('gulp-util');
-var sass = require('gulp-sass');
-var jade = require('gulp-jade');
-var concat = require('gulp-concat');
-var insert = require('gulp-insert');
-var tap = require('gulp-tap');
-var del = require('del');
-
-var insertToJST = function(file) {
-  var templateName = file.relative.replace(/\.js/, '');
-  file.contents = Buffer.concat([
-    new Buffer("window.JST['" + templateName + "'] = "),
-    file.contents,
-    new Buffer(';')
-  ]);
-};
-
-gulp.task('clean', function() {
-  del([
-    'public/javascripts',
-    'public/stylesheets',
-    'public/templates.js'
-  ]);
-});
-
-gulp.task('templates', function() {
-  gulp.src('./app/assets/templates/**/*.jade')
-    .pipe(jade({client: true}))
-    .pipe(tap(insertToJST))
-    .pipe(concat('templates.js'))
-    .pipe(insert.prepend('window.JST = {};\n'))
-    .pipe(gulp.dest('./public'))
-});
-
-gulp.task('coffee', function() {
-  gulp.src('./app/assets/javascripts/**/*.coffee')
-    .pipe(coffee({bare: true})).on('error', gutil.log)
-    .pipe(gulp.dest('./public/javascripts'));
-});
-
-gulp.task('scss', function() {
-  gulp.src('./app/assets/stylesheets/**/*.scss')
-    .pipe(sass())
-    .pipe(gulp.dest('./public/stylesheets/'));
-});
-
-gulp.task('build', ['clean', 'templates', 'coffee', 'scss']);
-
-gulp.task('watch', function() {
-  gulp.watch('app/assets/javascripts/**/*', ['build']);
-  gulp.watch('app/assets/stylesheets/**/*', ['build']);
-  gulp.watch('app/assets/templates/**/*', ['build']);
-});
 
 gulp.task('server', function() {
   nodemon({
     script: './bin/www'
   });
 });
-
-gulp.task('develop', ['build', 'watch', 'server']);
